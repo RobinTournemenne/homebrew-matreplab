e2872e37d9368cefca575fdc9dc0f2264372b276
robin tournemenne <rtournem@lavabit.com>
2020-03-29 21:02:31 +0200

    new sha256

--- HomebrewFormula/matreplab.rb
+++ HomebrewFormula/matreplab.rb
@@ -4,7 +4,7 @@ class Matreplab < Formula
   desc "an improved matlab -nodesktop"
   homepage "https://github.com/RobinTournemenne/matREPLab"
   url "https://github.com/RobinTournemenne/homebrew-matreplab/raw/homebrew/dist/matreplab-0.5.tar.gz"
-  sha256 "e9f4cd3fa75679e7ae0abecd020043543647c18f1aa6f054f860ff55efe5ca2a"
+  sha256 "cd5d5be8158e0119363c91a8a3f10ae6caa966b8fa39a632ab9d8854a1541029"
   version "0.5"
 
   depends_on "python"
